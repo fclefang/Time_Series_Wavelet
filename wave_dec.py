3b8d89a44fa6e2c50b86ac26fa718ff652d40274
MR-Fclef <1121181304@qq.com>
2018-06-11 22:26:10 +0800

    "wave_dec huarui_5min"

--- wave_dec.py
+++ wave_dec.py
@@ -2,31 +2,31 @@ import pandas as pd
 # # from pandas import read_csv
 # import pywt
 
-dataframe = pd.read_csv('foshan.csv', usecols=[1], engine='python', encoding='utf-8')
+dataframe = pd.read_csv('huarui_5min.csv', usecols=[1], engine='python', encoding='utf-8')
 dataset = dataframe.values
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pywt
-x = dataset[:960,0]
+x = dataset[190:220,0]
 plt.plot(x)
 plt.show()
 db1 = pywt.Wavelet('db4')
 # level = pywt.dwt_max_level(len(x), db1)
-cA7,cD7,cD6,cD5,cD4,cD3,cD2,cD1 = pywt.wavedec(x, db1, mode='constant', level=7)
+cA2,cD2,cD1 = pywt.wavedec(x, db1, mode='constant', level=2)
 plt.plot(cD1)
 plt.show()
 plt.plot(cD2)
 plt.show()
-plt.plot(cD3)
+# plt.plot(cD3)
+# plt.show()
+# plt.plot(cD4)
+# plt.show()
+plt.plot(cA2)
 plt.show()
-plt.plot(cD4)
-plt.show()
-plt.plot(cD5)
-plt.show()
-plt.plot(cD6)
-plt.show()
-plt.plot(cD7)
-plt.show()
+# plt.plot(cD6)
+# plt.show()
+# plt.plot(cD7)
+# plt.show()
 # plt.plot(cD8)
 # plt.show()
